c0b60f1f1133f3a94990737988f87479fa34a9db
nntcuong <thecuongyt3htk@gmail.com>
2024-02-21 21:47:11 +0700

    test

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname,'public')));
 
 
 app.get('/', (req, res) => {
