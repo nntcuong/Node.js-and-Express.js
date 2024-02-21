e1d991ebcb387764ed40c3d192e04145895598f1
nntcuong <thecuongyt3htk@gmail.com>
2024-02-21 21:43:06 +0700

    test

--- src/server.js
+++ src/server.js
@@ -10,8 +10,11 @@ require('dotenv').config();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname,'public')))
+
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World nodemon');
 });
 
 app.get('/abc', (req, res) => {
