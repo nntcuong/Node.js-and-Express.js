6f1ac12e7b001dcf8f4fa08fac8b8feb0a76e332
nntcuong <thecuongyt3htk@gmail.com>
2024-02-22 09:53:27 +0700

    test

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,15 @@
 const getHomepage=(req,res)=>{
     res.send('Hello World Cuong')
 }
+const getABC=(req,res)=>{
+    res.send('check ABC')
+}
+const getHoiDanIT=(req,res)=>{
+    // res.send(`<h1> Hoi dan it </h1>`);
+    res.render('sample.ejs');
+}
 module.exports={
-    getHomepage
+    getHomepage,
+    getABC,
+    getHoiDanIT
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,18 +1,14 @@
 const express = require('express');
-const { getHomepage } = require('../controllers/homeController');
+const { getHomepage,getABC,getHoiDanIT } = require('../controllers/homeController');
 
 const router=express.Router();
 
 
 router.get('/',getHomepage);
 
-router.get('/abc', (req, res) => {
-    res.send('check ABC');
-});
+router.get('/abc',getABC);
 
-router.get('/hoidanit', (req, res) => {
-    // res.send(`<h1> Hoi dan it </h1>`);
-    res.render('sample.ejs');
-});
+
+router.get('/hoidanit',getHoiDanIT);
 
 module.exports = router;
