d08087cecbf233cd862c3607b565d538f1a93899
nntcuong <thecuongyt3htk@gmail.com>
2024-03-01 23:09:28 +0700

    test

--- src/controllers/apiControllers.js
+++ src/controllers/apiControllers.js
@@ -7,6 +7,52 @@ const getUsersAPI = async (req, res) => {
         data: results
     })
 }
+
+const postCreateUserAPI = async (req, res) => {
+
+    let email = req.body.email;
+    let name = req.body.name;
+    let city = req.body.city;
+    let user = await User.create({
+        email: email,
+        name: name,
+        city: city
+    })
+       
+ 
+    return res.status(200).json({
+        EC: 0,
+        data: user
+    })
+
+}
+
+const putUpdateUserAPI = async (req, res) => {
+    console.log("req ==>", req.body);
+    let email = req.body.email;
+    let name = req.body.name;
+    let city = req.body.city;
+    let userId = req.body.userId;
+    let user = await User.updateOne({_id:userId},{name:name,email:email,city:city});
+    return res.status(200).json({
+        EC: 0,
+        data: user
+    })
+}
+const deleteUserAPI=async ( req, res) => {
+    const id=req.body.userId
+    await User.deleteOne({
+        _id: id
+    })
+    return res.status(200).json({
+        EC: 0,
+        data: User
+    })
+}
 module.exports = {
-    getUsersAPI
+    getUsersAPI,
+    postCreateUserAPI,
+    putUpdateUserAPI,
+    deleteUserAPI,
+
 }
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,16 +1,12 @@
 const express = require('express');
 
 const routerAPI=express.Router();
-const {getUsersAPI}=require('../controllers/apiControllers')
+const {getUsersAPI,postCreateUserAPI,putUpdateUserAPI,deleteUserAPI}=require('../controllers/apiControllers')
 
-routerAPI.get('/',(req,res)=>{
-    res.send("Hello world with apis")
-});
-routerAPI.get('/abc',(req,res)=>{
-    res.status(200).json({
-        data: 'Hello with api'
-    })
-});
+
+
 routerAPI.get('/users',getUsersAPI);
-
+routerAPI.post('/users',postCreateUserAPI);
+routerAPI.put('/users',putUpdateUserAPI);
+routerAPI.delete('/users',deleteUserAPI);
 module.exports = routerAPI;
