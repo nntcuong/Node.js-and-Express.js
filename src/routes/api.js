eb77a8de2347454ab8b153364e853eb752a8affa
nntcuong <thecuongyt3htk@gmail.com>
2024-03-01 17:13:44 +0700

    test

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,11 +1,16 @@
 const express = require('express');
 
 const routerAPI=express.Router();
-
+const {getUsersAPI}=require('../controllers/apiControllers')
 
 routerAPI.get('/',(req,res)=>{
     res.send("Hello world with apis")
 });
-
+routerAPI.get('/abc',(req,res)=>{
+    res.status(200).json({
+        data: 'Hello with api'
+    })
+});
+routerAPI.get('/users',getUsersAPI);
 
 module.exports = routerAPI;
